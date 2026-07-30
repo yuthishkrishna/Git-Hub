8d93721b079c3e15ae60bf641eaba9f96d1318a7
yuthishkrishna <yuthishkrishnah8@gmail.com>
2026-07-30 22:45:42 +0530

    tower of hanoi

---
+++ my free code camp practice projects/tower_of_hanoi.py
@@ -0,0 +1,33 @@
+def hanoi_solver(disks):
+    one = [i for i in range(disks, 0, -1)]
+    two = []
+    three = []
+    
+    def get_the_list():
+        return f'{one} {two} {three}\n'
+    
+    def move_the_disk(start, target):
+        if not start:
+            return
+        target.append(start.pop())
+
+    def move_the_tower(start, target, auxillary, disks):
+        if disks == 1:
+            move_the_disk(start, target)
+            return get_the_list()
+
+        result = ""
+        result += move_the_tower(start, auxillary, target, disks - 1)
+
+        move_the_disk(start, target)
+        result += get_the_list()
+
+        result += move_the_tower(auxillary, target, start, disks - 1)
+        return result
+
+    return_string = get_the_list()
+    if disks > 0:
+        return_string += move_the_tower(one, three, two,disks )
+        return return_string.rstrip("\n")
+
+print(hanoi_solver(2))
